5016d51f0c741ad159920d5f5a46eb4de34de185
S00160641 <sophiaprice10@hotmail.com>
2017-10-27 10:19:32 +0100

    Initital Console App with LINQ Queries
    Work Completed 27/10/2017

--- LinqExampleGroup/Program.cs
+++ LinqExampleGroup/Program.cs
@@ -50,19 +50,71 @@ namespace LinqExampleGroup
                 _xp = value;
             }
         }
+
+        public override string ToString()
+        {
+            return _id.ToString() + " " + _name.ToString() + " " + _xp.ToString();
+        }
     }
     class Program
     {
-        List<Player> players = new List<Player>()
+        static List<Player> players = new List<Player>()
         {
             new Player { Id = Guid.NewGuid(), Name = "Pete Volga", Xp = 100 },
-            new Player { Id = Guid.NewGuid(), Name = "Joe Bloggs", Xp = 200 },
+            new Player { Id = Guid.NewGuid(), Name = "Joe Bloggs", Xp = 20 },
             new Player { Id = Guid.NewGuid(), Name = "Pete Townsend", Xp = 150 },
             new Player { Id = Guid.NewGuid(), Name = "Mary Shelly", Xp = 180 },
             new Player { Id = Guid.NewGuid(), Name = "Jane Doe", Xp = 105 }
         };
         static void Main(string[] args)
         {
+            // Return the first occurance of the match or null
+            Console.WriteLine("Jane Doe Stats:");
+            Player found = players.FirstOrDefault(p => p.Name == "Jane Doe");
+            if (found != null)
+                Console.WriteLine("{0}", found.ToString());
+            else
+                Console.WriteLine("Player Not Found");
+
+            Console.WriteLine();
+
+            // Return the first occurance of the some records
+            Console.WriteLine("Pete Stats:");
+            Player matchPlayers = players.FirstOrDefault(p => p.Name.Contains("Pete"));
+            if (matchPlayers != null)
+                Console.WriteLine("First Found: {0}", matchPlayers.ToString());
+            else
+                Console.WriteLine("Player Not Found");
+
+            Console.WriteLine();
+
+            // Return all those with an XP value over 100
+            Console.WriteLine("Top Players:");
+            List<Player> topPlayers = players.Where(p => p.Xp > 100).ToList();
+
+            if (topPlayers.Count > 0)
+            {
+                foreach (var item in topPlayers)
+                {
+                    Console.WriteLine("{0}", item.ToString());
+                }
+            }
+            else
+                Console.WriteLine("No Players with XP over 100");
+            Console.WriteLine("");
+
+            // Produce a ScoreBoard of Player Names and Scores
+            Console.WriteLine("Score Board:");
+            var ScoreBoard = players
+                                .OrderByDescending(o => o.Xp)
+                                .Select(scores => new { scores.Name, scores.Xp });
+
+            foreach (var item in ScoreBoard)
+            {
+                Console.WriteLine("{0} {1}", item.Name, item.Xp);
+            }
+
+            Console.ReadKey();
         }
     }
 }
